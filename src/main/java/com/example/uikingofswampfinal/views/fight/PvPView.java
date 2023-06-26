9541a2f1f7884d4319ab24429176d6c73a2ab1a9
Piotr Szegda <piotrszegda@protonmail.com>
2023-06-26 18:35:09 +0200

    Fight rework

--- src/main/java/com/example/uikingofswampfinal/views/fight/PvPView.java
+++ src/main/java/com/example/uikingofswampfinal/views/fight/PvPView.java
@@ -19,8 +19,8 @@ import java.math.BigDecimal;
 @PageTitle("Fight PVP")
 @Route(value = "fight-pvp", layout = MainLayout.class)
 public class PvPView extends Composite<VerticalLayout> {
-	private final ComboBox<SamplePlayer> playerA = new ComboBox<>("Attacking player.");
-	private final ComboBox<SamplePlayer> playerB = new ComboBox<>("Defending player.");
+	private final ComboBox<SamplePlayer> playerA = new ComboBox<>("Player A");
+	private final ComboBox<SamplePlayer> playerB = new ComboBox<>("Player B");
 	private SamplePlayerService samplePlayerService;
 	
 	private final int HEALTH_OF_TROOPS = 2;
@@ -42,59 +42,80 @@ public class PvPView extends Composite<VerticalLayout> {
 		
 		getContent().add(endOfRoundButton);
 	}
-	
-	private void calculateFight(ClickEvent<Button> buttonClickEvent) {
-		var player_a = this.playerA.getValue();
-		var player_b = this.playerB.getValue();
-		// TODO: Jakoś zamieniać
-		
-		var attacking_player = player_a;
-		var defending_player = player_b;
-		
-		var attack_point = attacking_player.getTroopsamount()
-				.multiply(BigDecimal.valueOf(ATTACK_POINT_OF_TROOPS))
-				.add(attacking_player.getAttackpoints()).doubleValue();
-		
-		var defending_player_troops = defending_player.getTroopsamount().doubleValue();
-		var new_defending_player_troops = defending_player_troops;
-		var new_hero_live_point = defending_player.getHealth().doubleValue();
-		
-		if (defending_player_troops != 0) {
-			// Troops attack
-			int destroyedTroops = (int) Math.round(attack_point / HEALTH_OF_TROOPS);
-			new_defending_player_troops = defending_player_troops - destroyedTroops;
-			if (new_defending_player_troops < 0) {
-				new_defending_player_troops = 0;
-			}
-			Notification.show("Player:" + defending_player.getNickname()
-							+ ", lost troops: " +
-							destroyedTroops,
-					1000 * 10, Notification.Position.MIDDLE);
-		} else {
-			Notification.show("Hero attack:" + defending_player.getNickname(),
-					1000 * 10, Notification.Position.MIDDLE);
-			//Hero attack
-			var hero_live_point = defending_player.getHealth().doubleValue();
-			var hero_defend_point = defending_player.getDefensepoints().doubleValue();
-			
-			new_hero_live_point = hero_live_point - Math.abs(hero_defend_point - attack_point);
-			
-			if (new_hero_live_point <= 0) {
-				//Die
-				Notification.show("Player:" + defending_player.getNickname()
-								+ " die",
-						1000 * 10, Notification.Position.MIDDLE);
-				defending_player.setActive(false);
-				
-			}
-			
-		}
-		defending_player.setTroopsamount(BigDecimal.valueOf(new_defending_player_troops));
-		defending_player.setHealth(BigDecimal.valueOf(new_hero_live_point));
-		
-		samplePlayerService.update(defending_player);
-		playerB.setValue(defending_player);
-		
-		
+
+	private double calculateAttackPoints(SamplePlayer player) {
+		return player.getTroopsamount().multiply(BigDecimal.valueOf(ATTACK_POINT_OF_TROOPS))
+				.add(player.getAttackpoints()).doubleValue();
 	}
+
+	private int calculateDestroyedTroops(double attackPoints, double defendingTroops) {
+		return (int) Math.round(attackPoints / HEALTH_OF_TROOPS);
+	}
+
+	private double calculateHeroAttackPoint(SamplePlayer player) {
+		return player.getAttackpoints().doubleValue();
+	}
+
+
+	private void calculateFight(ClickEvent<Button> buttonClickEvent) {
+		var playerA = this.playerA.getValue();
+		var playerB = this.playerB.getValue();
+
+		var attackPointA = calculateAttackPoints(playerA);
+		var attackPointB = calculateAttackPoints(playerB);
+
+		var destroyedTroopsA = calculateDestroyedTroops(attackPointA, playerB.getTroopsamount().doubleValue());
+		var destroyedTroopsB = calculateDestroyedTroops(attackPointB, playerA.getTroopsamount().doubleValue());
+
+		var newTroopsAmountA = playerA.getTroopsamount().doubleValue() - destroyedTroopsA;
+		var newTroopsAmountB = playerB.getTroopsamount().doubleValue() - destroyedTroopsB;
+
+		if (newTroopsAmountA < 0) {
+			newTroopsAmountA = 0;
+		}
+
+		if (newTroopsAmountB < 0) {
+			newTroopsAmountB = 0;
+		}
+
+		Notification.show("Player: " + playerA.getNickname() + " lost troops: " + destroyedTroopsA, 1000 * 10, Notification.Position.MIDDLE);
+		Notification.show("Player: " + playerB.getNickname() + " lost troops: " + destroyedTroopsB, 1000 * 10, Notification.Position.MIDDLE);
+
+		playerA.setTroopsamount(BigDecimal.valueOf(newTroopsAmountA));
+		playerB.setTroopsamount(BigDecimal.valueOf(newTroopsAmountB));
+
+		if (newTroopsAmountA <= 0) {
+			performHeroAttack(playerA, playerB, attackPointB);
+		}
+
+		if (newTroopsAmountB <= 0) {
+			performHeroAttack(playerB, playerA, attackPointA);
+		}
+
+		samplePlayerService.update(playerA);
+		samplePlayerService.update(playerB);
+
+		this.playerA.setValue(playerA);
+		this.playerB.setValue(playerB);
+	}
+
+	private void performHeroAttack(SamplePlayer attackingPlayer, SamplePlayer defendingPlayer, double opponentAttackPoint) {
+		var heroAttackPoint = calculateHeroAttackPoint(attackingPlayer);
+		var newHeroLivePoint = defendingPlayer.getHealth().doubleValue() - Math.abs(heroAttackPoint - opponentAttackPoint);
+
+		if (newHeroLivePoint <= 0) {
+			endGame(defendingPlayer);
+			return;
+		}
+
+		defendingPlayer.setHealth(BigDecimal.valueOf(newHeroLivePoint));
+		Notification.show("Hero attack: " + defendingPlayer.getNickname(), 1000 * 10, Notification.Position.MIDDLE);
+	}
+
+	private void endGame(SamplePlayer player) {
+		player.setActive(false);
+		samplePlayerService.update(player);
+		Notification.show("Player: " + player.getNickname() + " has lost the game.", 1000 * 10, Notification.Position.MIDDLE);
+	}
+
 }
